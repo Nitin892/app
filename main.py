37e629e1f065b00182d9ef1125b071595f798614
Nitin892 <78948806+Nitin892@users.noreply.github.com>
2021-03-16 09:27:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.config import Config
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+from kivymd.app import MDApp
+from kivy.uix.textinput import TextInput
+from kivymd.uix.button import MDFlatButton
+
+from kivymd.uix.label import MDLabel #importing for text
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager   #import screen for managing screen
+import pyttsx3
+engine=pyttsx3.init("sapi5")
+
+
+class DemoApp(MDApp):
+    engine=pyttsx3.init("sapi5")
+
+    def build(self):
+
+        self.theme_cls.theme_style="Dark"
+        screen=Builder.load_file("demo.kv")
+        return screen
+
+
+    def speak(self):
+        self.engine.save_to_file(self.root.ids.text.text, "voice.mp3")
+        self.engine.runAndWait()
+
+    def demo(self):
+        self.engine.say(self.root.ids.text.text)
+        self.engine.runAndWait()
+
+# if __name__ == '__main__':
+
+DemoApp().run()
